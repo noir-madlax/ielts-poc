edb0cc2015a9a76d7042795b7803bbbdda402432
noir <zxj8911@hotmail.com>
2024-09-02 20:40:44 +0800

    fix white screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import HomePage from './HomePage';
 import EnglishLearningTopics from './pages/main/EnglishLearningTopics';
 import MessagingInterface from "./MessagingInterface";
